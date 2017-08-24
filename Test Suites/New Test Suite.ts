49a49e57f5e585b26ed31aa1180d107588e869fc
Pasha <Pasha@Home_PC>
2017-08-24 13:28:41 +0300

    New suite

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>72910db2-dc4a-4949-adc4-06ae482c6a82</testSuiteGuid>
+</TestSuiteEntity>
